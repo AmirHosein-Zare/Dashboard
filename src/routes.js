4d805b089eab4d6886c83a81060d00ef0a79c73e
AmirHosein <awir.software@gmail.com>
2023-01-27 14:43:10 +0330

    define Products Route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Home from "./pages/Home/Home"
 import UserList from "./pages/Users/UserList"
+import NewUser from "./pages/NewUser/NewUser"
 
 let routes = [
     {path:'/', element: <Home />},
     {path:'/users', element: <UserList />},
-    {path:'/newuser', element: <NewUSer />},
+    {path:'/newuser', element: <NewUser />},
+    {path:'/products', element: <Products />},
 ]
 
 export default routes
