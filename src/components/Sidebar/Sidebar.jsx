61a09540ea9738d6765751b5e8468f1a29fd8ff9
AmirHosein <awir.software@gmail.com>
2023-01-27 17:12:45 +0330

    add sidebar html code

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import './Sidebar.css'
+import LineStyleIcon from '@mui/icons-material/LineStyle';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+        <div className="sidebarwrapper">
+            <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Dashboard</h3>
+                <ul className="sidebarList">
+                    <li className="sidebarItem">
+                        <LineStyleIcon className='sidebarIcon'/>
+                        Home
+                    </li>
+                    <li className="sidebarItem">
+                        <TimelineIcon className='sidebarIcon'/>
+                        Analytics
+                    </li>
+                    <li className="sidebarItem">
+                        <TrendingUpIcon className='sidebarIcon'/>
+                        Sales
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
