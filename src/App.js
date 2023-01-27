cdc00d2eaff5d5eefdf1bf499b0ca77825b84e72
AmirHosein <awir.software@gmail.com>
2023-01-27 14:21:02 +0330

    get routes from routes module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useRoutes } from 'react'
+import routes from './routes'
 
 export default function App() {
     
+    let router = useRoutes(routes)
     
     return (
-        
+        <>
+        </>
     )
 }
