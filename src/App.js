abe799cb0a80aabf578e39d5120dac4e12645d3c
AmirHosein <awir.software@gmail.com>
2023-01-27 14:34:03 +0330

    edit useRoutes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useRoutes } from 'react'
+import React from 'react'
 import routes from './routes'
+import { useRoutes } from 'react-router-dom'
 
 export default function App() {
     
