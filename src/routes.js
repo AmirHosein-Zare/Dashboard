d12996225b3fd8e78f29b4c5e988e5b76db8a6bf
AmirHosein <awir.software@gmail.com>
2023-01-27 14:40:19 +0330

    define newUser Route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import Home from "./pages/home/Home"
+import UserList from "./pages/Users/UserList"
 
 let routes = [
     {path:'/', element: <Home />},
     {path:'/users', element: <UserList />},
-
+    {path:'/newuser', element: <NewUSer />},
 ]
 
 export default routes
