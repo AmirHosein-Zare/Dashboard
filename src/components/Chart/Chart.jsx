5e0fb41b10f861f12dc6604a77f2ce53f62ca5aa
AmirHosein <awir.software@gmail.com>
2023-01-27 21:54:43 +0330

    finish chart and final edit

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -16,7 +16,7 @@ export default function Chart({title, data, dataKey, grid}) {
         <ResponsiveContainer width="100%" aspect={4}>
             <LineChart data={data}>
                 <XAxis dataKey="name" stroke="#5550bd"/>
-                <Line dataKey={dataKey} stroke="#5550bd" />
+                <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
                 <Tooltip />
                 {grid && <CartesianGrid stroke='e0dfdf' strokeDasharray="10" />}
             </LineChart>
