6ba4b555410faa583fc637d64e4f4e316761d981
AmirHosein <awir.software@gmail.com>
2023-01-27 20:43:49 +0330

    add datas for chart

---
+++ src/datas.js
@@ -0,0 +1,50 @@
+let xAxisData = [
+    {
+        "name": "Jan",
+        "Sale": 112_000
+    },
+    {
+        "name": "Feb",
+        "Sale": 99_000
+    },
+    {
+        "name": "Mar",
+        "Sale": 12_090
+    },
+    {
+        "name": "Apr",
+        "Sale": 99_000
+    },
+    {
+        "name": "May",
+        "Sale": 54_000
+    },
+    {
+        "name": "Jun",
+        "Sale": 85_000
+    },
+    {
+        "name": "Jul",
+        "Sale": 34_000
+    },
+    {
+        "name": "Agu",
+        "Sale": 18_598
+    },
+    {
+        "name": "Sep",
+        "Sale": 0
+    },
+    {
+        "name": "Oct",
+        "Sale": 73_078
+    },
+    {
+        "name": "Nov",
+        "Sale": 12_900
+    },
+    {
+        "name": "Dec",
+        "Sale": 97_000
+    },
+]
