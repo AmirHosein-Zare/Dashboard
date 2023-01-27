e308246fefae4e87cc8f6adad7e69638c151c79f
AmirHosein <awir.software@gmail.com>
2023-01-27 22:08:05 +0330

    edit html widgetSm

--- src/components/WidgetSm/WidgetSm.jsx
+++ src/components/WidgetSm/WidgetSm.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './WidgetSm.css'
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 export default function WidgetSm() {
   return (
-    <div>WidgetSm</div>
+    <div className='WidgetSm'>
+        <span className='WidgetSmTitle'>New User Joined</span>
+        <div className="WidgetSmList">
+            <li className="WidgetSmItem">
+                <img src="logo512.png" />
+                <div className="WidgetSmUser">
+                    <span className='WidgetSmUserName'>Qadir Yolme</span>
+                    <span className='WidgetSmUserTitle'>Hacker</span>
+                </div>
+                <button className='WidgetSmBtn'>
+                    <VisibilityIcon className='WidgetSmIcon' />
+                </button>
+            </li>
+        </div>
+    </div>
   )
 }
