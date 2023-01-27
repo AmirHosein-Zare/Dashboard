6a7bdc51b46122c89071f8d80a3fb61ee7e569db
AmirHosein <awir.software@gmail.com>
2023-01-27 22:36:16 +0330

    add users in Widget

--- src/components/WidgetSm/WidgetSm.jsx
+++ src/components/WidgetSm/WidgetSm.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './WidgetSm.css'
 import WidgetSmItem from './WidgetSmItem/WidgetSmItem';
+import { users } from '../../datas';
 
 export default function WidgetSm() {
   return (
     <div className='WidgetSm'>
         <span className='WidgetSmTitle'>New User Joined</span>
-        <WidgetSmItem />
-        <WidgetSmItem />
-        <WidgetSmItem />
-        <WidgetSmItem />
+        {users.map(user => (
+            <WidgetSmItem key={user.id} {...user}/>
+        ))}
     </div>
   )
 }
--- src/components/WidgetSm/WidgetSmItem/WidgetSmItem.jsx
+++ src/components/WidgetSm/WidgetSmItem/WidgetSmItem.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import './WidgetSmItem.css'
 import VisibilityIcon from '@mui/icons-material/Visibility';
 
-export default function WidgetSmItem() {
+export default function WidgetSmItem(props) {
   return (
     <div>
         <ul className="WidgetSmList">
             <li className="WidgetSmItem">
-                <img src="logo512.png" className='WidgetSmImg'/>
+                <img src={props.img} className='WidgetSmImg'/>
                 <div className="WidgetSmUser">
-                    <span className='WidgetSmUserName'>Qadir Yolme</span>
-                    <span className='WidgetSmUserTitle'>Hacker</span>
+                    <span className='WidgetSmUserName'>{props.name}</span>
+                    <span className='WidgetSmUserTitle'>{props.job}</span>
                 </div>
                 <button className='WidgetSmBtn'>
                     <VisibilityIcon className='WidgetSmIcon' />
