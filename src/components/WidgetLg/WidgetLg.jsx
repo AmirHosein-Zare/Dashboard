1f83db19d75b0027c20cf2a6861098b700db393b
AmirHosein <awir.software@gmail.com>
2023-01-28 00:33:51 +0330

    add widget html code

--- src/components/WidgetLg/WidgetLg.jsx
+++ src/components/WidgetLg/WidgetLg.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 import './WidgetLg.css'
 
 export default function WidgetLg() {
-  return (
-    <div>WidgetLg</div>
+  
+    const Button = ({type}) => {
+        return <button className={'WidgetLgBtn' + type}>{type}</button>
+    }
+  
+    return (
+    <div className='WidgetLg'>
+        <h3 className='WidgetLgTitle'>Lastest Transactios</h3>
+        <table className="WidgetLgTable">
+            <tr className="WidgetLgTr">
+                <th className="WidgetLgTh">Customer</th>
+                <th className="WidgetLgTh">Date</th>
+                <th className="WidgetLgTh">Amount</th>
+                <th className="WidgetLgTh">Status</th>
+            </tr>
+            <tr>
+                <td className='WidgetLgTd WidgetLgUser'>
+                    <img src="logo512.png" className='WidgetLgImg'/>
+                    <span>Qadir Yolme</span>
+                </td>
+                <td className='WidgetLgDate WidgetLgTd'>2 May 2023</td>
+                <td className='WidgetLgPrice WidgetLgTd'>$199.95</td>
+                <td className='WidgetLgStatus WidgetLgTd'>
+                    <Button type="Approved" />
+                </td>
+            </tr>
+        </table>
+    </div>
   )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,7 @@ import Feature from '../../components/Feature/Feature'
 import Chart from '../../components/Chart/Chart'
 import {xAxisData} from './../../datas'
 import WidgetSm from '../../components/WidgetSm/WidgetSm'
+import WidgetLg from '../../components/WidgetLg/WidgetLg'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       <Chart grid title='Month Sale' data={xAxisData} dataKey='Sale' />
       <div className='homeWidget'>
         <WidgetSm />
-        
+        <WidgetLg />
       </div>
     </div>
   )
