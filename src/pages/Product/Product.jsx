3494745ad587207146dd2912a26e8de26ef13745
AmirHosein <awir.software@gmail.com>
2023-01-28 15:07:04 +0330

    add title for product

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react'
 import './Product.css'
 import { ShowStuff } from '../../datas';
-import { useRoutes } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 export default function Product() {
     const [products, setProducts] = useState(ShowStuff)
-    const productId = useRoutes('productID')
+    const productId = useParams('productID')
 
 
   return (
     <div className='Product'>
-
+        <div className="productTitleContainer">
+            <h1 className="productTitle">Product</h1>
+            <Link to="/products" className='link'>
+                <Button variant="contained">Create</Button>
+            </Link>
+        </div>
     </div>
   )
 }
