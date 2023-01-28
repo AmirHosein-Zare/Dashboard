05422d1b83c83aee132ce41de44f2168c4ddc78d
AmirHosein <awir.software@gmail.com>
2023-01-28 20:03:11 +0330

    add new user form

--- src/pages/NewUser/NewUser.js
+++ src/pages/NewUser/NewUser.js
@@ -1,8 +1,51 @@
 import React from 'react'
 import './NewUser.css'
+import TextField from '@mui/material/TextField';
+import Fab from '@mui/material/Fab';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import Button from '@mui/material/Button';
 
 export default function NewUser() {
   return (
-    <div>NewUser</div>
+    <div className='newUser'>
+      <div className="newUserTitle">
+        <h1>Add New User</h1>
+      </div>
+      <div className="newUserForm">
+      <TextField
+          required
+          id="outlined-required"
+          label="Name"
+          defaultValue=""
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="User Name"
+          defaultValue=""
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Job"
+          defaultValue=""
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Age"
+          defaultValue=""
+        />
+        <div className='newUserPic'>
+          <span className='newUserPicTitle'>Add Avatar</span>
+        <Fab color="primary" aria-label="add" className='newUserAddPic'>
+          <AddPhotoAlternateIcon />
+        </Fab>
+        </div>
+        <Button className='newUserBtn' variant="contained" color="success">
+          Register
+        </Button>
+      </div>
+    </div>
   )
 }
