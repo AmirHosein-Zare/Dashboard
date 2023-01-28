c381d87886bbe29e49082a2635bd87a9508037fd
AmirHosein <awir.software@gmail.com>
2023-01-28 13:05:45 +0330

    add UserList html

--- src/pages/Users/UserList.js
+++ src/pages/Users/UserList.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { userRows } from '../../datas';
 import './UserList.css'
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
 
 export default function UserList() {
+
+    const [UserDatas, setUserDatas] = useState(userRows)
+
+    //data Grid Column
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        {
+          field: 'user',
+          headerName: 'User',
+          width: 300,
+          renderCell: (params) => {
+            return(
+              <Link to='/' className='UserListLink'>
+                <div className="UserListUser">
+                  <img src={params.row.avatar} className="UserListImg"/>
+                  {params.row.username}
+                </div>
+              </Link>
+            )
+          }
+        },
+        {
+          field: 'status',
+          headerName: 'Status',
+          width: 150,
+        },
+        {
+          field: 'transaction',
+          headerName: 'Transaction',
+          width: 110,
+        },
+      ];
+
   return (
-    <div>UserList</div>
+    <div className='UserList'>
+        <Box sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+              rows={UserDatas}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+              disableSelectionOnClick
+              experimentalFeatures={{ newEditingApi: true }}
+            />
+        </Box>
+    </div>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import Home from "./pages/Home/Home"
-import UserList from "./pages/Users/UserList"
+import Users from "./pages/Users/UserList"
 import NewUser from "./pages/NewUser/NewUser"
 import Products from "./pages/Products/Products"
 
 let routes = [
     {path:'/', element: <Home />},
-    {path:'/users', element: <UserList />},
+    {path:'/users', element: <Users />},
     {path:'/newuser', element: <NewUser />},
     {path:'/products', element: <Products />},
 ]
