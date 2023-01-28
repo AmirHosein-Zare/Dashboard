826a410b4021f00afe892db9e26e0ec06af55675
AmirHosein <awir.software@gmail.com>
2023-01-28 14:56:57 +0330

    get product id from route

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Product.css'
+import { ShowStuff } from '../../datas';
+import { useRoutes } from 'react-router-dom';
 
 export default function Product() {
+    const [products, setProducts] = useState(ShowStuff)
+    const productId = useRoutes('productID')
+
+
   return (
     <div className='Product'>
 
