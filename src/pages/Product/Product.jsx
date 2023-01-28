6ffcb5d1fb301953dd812b43dba1def6a851de58
AmirHosein <awir.software@gmail.com>
2023-01-28 15:13:01 +0330

    add chart for product

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -3,6 +3,7 @@ import './Product.css'
 import { ShowStuff } from '../../datas';
 import { Link, useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import Chart from './../../components/Chart/Chart'
 
 export default function Product() {
     const [products, setProducts] = useState(ShowStuff)
@@ -11,11 +12,21 @@ export default function Product() {
 
   return (
     <div className='Product'>
+
         <div className="productTitleContainer">
             <h1 className="productTitle">Product</h1>
             <Link to="/products" className='link'>
                 <Button variant="contained">Create</Button>
             </Link>
+        </div>
+
+        <div className="productTop">
+            <div className="productTopLeft">
+                <Chart title="Sale In Month" grid={true} data={} dataKey="Sale"/>
+            </div>
+            <div className="productTopRight">
+
+            </div>
         </div>
     </div>
   )
