deba31fa072eaf7ecb6a0ba388418d3ef1e2cdac
AmirHosein <awir.software@gmail.com>
2023-01-28 14:52:04 +0330

    add product component

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import Home from "./pages/Home/Home"
 import Users from "./pages/Users/UserList"
 import NewUser from "./pages/NewUser/NewUser"
 import Products from "./pages/Products/Products"
+import Product from "./pages/Product/Product"
 
 let routes = [
     {path:'/', element: <Home />},
     {path:'/users', element: <Users />},
     {path:'/newuser', element: <NewUser />},
     {path:'/products', element: <Products />},
+    {path:'/products/:productID', element: <Product />},
 ]
 
 export default routes
